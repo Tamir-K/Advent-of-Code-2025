bce80d79dcb022f4238b2e8686133118679b6f2a
Tamir-K <121674805+Tamir-K@users.noreply.github.com>
2025-12-12 19:48:31 +0200

    Improve day 6 Solution

--- day06_part1.py
+++ day06_part1.py
@@ -2,18 +2,20 @@
 
 # Imports
 import fileinput
-import numpy as np
 
 # Constants
 SIGN = -1
 
 
+def transpose(matrix):
+    "Swap the rows and columns of a 2-D matrix."
+    return zip(*matrix, strict=True)
+
+
 def main():
     puzzle = [row.strip().split() for row in fileinput.input()]
-    rearranged_puzzle = np.array(puzzle).T
-    problems = (row[SIGN].join(row[:SIGN]) for row in rearranged_puzzle)
-    results = (eval(problem) for problem in problems)
-    print(sum(results))
+    problems = (row[SIGN].join(row[:SIGN]) for row in transpose(puzzle))
+    print(sum(map(eval, problems)))
 
 
 if __name__ == "__main__":
--- day06_part2.py
+++ day06_part2.py
@@ -2,36 +2,28 @@
 
 # Imports
 import fileinput
-import numpy as np
-import re
+from itertools import takewhile
 
 # Constants
+FIRST_ROW = 0
 SIGN = -1
-SPACE = " "
-ADDITION = "+"
+
+
+def transpose(matrix):
+    "Swap the rows and columns of a 2-D matrix."
+    return zip(*matrix, strict=True)
 
 
 def main():
-    puzzle = [re.split(r"", row.rstrip("\n")) for row in fileinput.input()]
-    rearranged_puzzle = np.array(puzzle).T
-
-    sign = None
-    result = 0
+    puzzle = [list(row.rstrip("\n")) for row in fileinput.input()]
+    rearranged_puzzle = transpose(puzzle)
     total_sum = 0
-    for row in rearranged_puzzle:
-        if not (np.any(row != SPACE) & np.any(row)):
-            total_sum += result
-            sign = None
-            result = 0
-        elif sign is None:
-            sign = row[SIGN]
-            result = int("".join(row[:SIGN]))
-        else:
-            result = (
-                result + int("".join(row))
-                if sign == ADDITION
-                else result * int("".join(row))
-            )
+    while problem := list(
+        takewhile(lambda row: any(char.strip() for char in row), rearranged_puzzle)
+    ):
+        sign = problem[FIRST_ROW][SIGN]
+        problem = sign.join("".join(row[:SIGN]) for row in problem)
+        total_sum += eval(problem)
     print(total_sum)
 
 
